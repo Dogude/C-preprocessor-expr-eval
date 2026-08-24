397dadd1266f8458a7f0d1b9f496d7ca20237659
polaris <131747396+Dogude@users.noreply.github.com>
2026-08-24 13:46:06 +0200

    Refactor c_eval function and improve syntax checks

--- eval.py
+++ eval.py
@@ -1,86 +1,165 @@
-def c_eval(expression,line_number):
+import re
 
-    line_number = line_number + 1  # Adjust line number for user-friendly reporting
+class Types:
+    OPERAND = 1
+    OPERATOR = 2
+    C = 3
+    D = 4
+    E = 5
+    F = 6
+    G = 7
+    
+def c_eval(expression):
 
     # expression is a string
     # this function returns True or False based on the evaluation of the expression
-
-
-    # lexical analysis - tokenize the expression
-    invalid_operators = ('++','{', '}' , '$' , '@' ,'--', '+=', '-=', '*=', '/=', '%=', '&=', '|=', '^=', '>>=', '<<=')
-
-    for op in invalid_operators:
-        if op in expression:
-            return "Error : Invalid operator in expression - " + op
-
-    stringList = re.findall(r'(!defined)\((\w+)\)\s*|(defined)\((\w+)\)\s*|(!defined)\s+(\w+)\s*|(\w+)|(&&|\|\||==|!=|\(|\)|\
-                            &|\||!|<<|>>|<|>|>=|<=|\+|-|/|%|\*|)\s*', expression)
-
+    stringList = re.findall(r'\b(defined)\b|(\w+)|(&&|\|\||==|!=|\(|\)|\
+                            &|\||!|<<|>>|<|>|>=|<=|\+|-|/|%|\*|^|~|.+?)', expression)
+    
     # convert list of tuples to list of strings
     tokens = []
-
+            
     for tup in stringList:
         for item in tup:
-            if item != '':
+            if item != '' and item != ' ':
                 tokens.append(item)
 
+    state = Types.OPERAND
+    
+    par = []
     # syntax checking & parsing
-    for i in range(len(tokens)):
-        # check defined and !defined syntax
-        if tokens[i] in ('defined','!defined'):
-            if tokens[i+1].isidentifier() == False and tokens[i+1].isnumeric() == False:
-                print("Error : Invalid expression syntax after defined or !defined, at line " + str(line_number))
-                sys.exit(1)
-        # check counts of opening and closing parentheses
-        open_parens = tokens.count('(')
-        close_parens = tokens.count(')')
-        if open_parens != close_parens:
-            print("Error : Mismatched parentheses in expression, at line " + str(line_number))
-            sys.exit(1)
-        # There must be a identifier or number before and after binary operators
-        if tokens[i] in ('==', '!=', '<', '>', '<=', '>=', '+' , '*', '&' , '&&' , '||' , '|', '/', '%', '<<', '>>'):
-            if i == 0 or i == len(tokens) - 1:
-                print("Error : Invalid expression syntax - operator at start or end, at line " + str(line_number))
-                sys.exit(1)
-            if not (tokens[i-1].isidentifier() or tokens[i-1].isnumeric() or tokens[i-1] in ('(', ')','-', 'defined', '!defined', '!')):
-                print("Error : Invalid expression syntax - operator before, at line " + str(line_number))
-                sys.exit(1)
-            if not (tokens[i+1].isidentifier() or tokens[i+1].isnumeric() or tokens[i+1] in ('(', ')', 'defined', '!defined', '-','!')):
-                print("Error : Invalid expression syntax - operator after, at line " + str(line_number))
-                sys.exit(1)
-        if tokens[i] == '!':
-            if i == len(tokens) - 1:
-                print("Error : Invalid expression syntax - ! operator at end, at line " + str(line_number))
-                sys.exit(1)
-            if not (tokens[i+1].isidentifier() or tokens[i+1].isnumeric() or tokens[i+1] in ('(', 'defined', '!defined')):
-                print("Error : Invalid expression syntax - ! operator before, at line " + str(line_number))
-                sys.exit(1)
-        if tokens[i] == '-':
-            if i < len(tokens) - 1:
-                if not (tokens[i+1].isidentifier() or tokens[i+1].isnumeric() or tokens[i+1] in ('(', 'defined', '!defined')):
-                    print("Error : Invalid expression syntax - - operator before, at line " + str(line_number))
-                    sys.exit(1)
+    for i in range(len(tokens)): 
+        t = tokens[i]
+        match state:
+            case Types.OPERAND:
+                if t == '!':
+                    pass
+                elif t == 'defined':
+                    state = Types.D
+                elif t == '-':
+                     tokens[i] = '-u' # convert unary sub
+                     state = Types.C
+                     
+                elif t == '+':
+                    tokens[i] = '+u'  # convert unary add
+                    state = Types.C
+                elif t == '~':
+                    state = Types.C
+                elif t == '(':
+                    par.append(1)
+                elif t.isnumeric() or t.isidentifier():
+                    state = Types.OPERATOR                   
+                else:
+                    print("error in expression1")
+                    return 
+            
+            case Types.D:
+                if t == '(':
+                    state = Types.E
+                elif t.isidentifier():
+                    state = Types.OPERATOR
+                else:
+                    print("error in expression2")
+                    return 
+            
+            case Types.E:
+                if t.isidentifier():
+                    state = Types.G
+                else:
+                    print("error in expression3")
+                    return 
+            
+            case Types.G:
+                if t == ')':
+                    state = Types.OPERATOR
+                else:
+                    print("error in expression4")
+                    return 
+            
+            case Types.C:
+                if t == '!':
+                    pass
+                elif t == '-':
+                     tokens[i] = '-u' # convert unary sub
+                elif t == '+':
+                    tokens[i] = '+u'  # convert unary add
+                elif t == '~':
+                    pass
+                elif t == '(':
+                    state = Types.OPERAND
+                    par.append(1)
+                elif t.isnumeric() or t.isidentifier():
+                    state = Types.OPERATOR
+                else:
+                    print("error in expression5")
+                    return 
+            
+            case Types.OPERATOR:
+                if t in ('&&' , '||' , '==' , '!=' , '&' ,  '|'  , '<<' , '>>' , '<' , '>' , '>=' , '<=' ,  '+' , '-' , '/' , '%' , '*' , '^'):
+                    state = Types.OPERAND
+                elif t == ')':
+                    if par is None:
+                        print("error in expression6")
+                        return
+                    par.pop()    
+                else:
+                    print("error in expression7")
+                    return
+    
+    if state != Types.OPERATOR:     
+        print("error in expression8")
+        return
+    elif par:
+        print("error in expression9")
+        return
 
-        if tokens[i] not in ('defined','!defined'):
-            if (tokens[i].isidentifier() or tokens[i].isnumeric()) and i < len(tokens) - 1:
-                if(tokens[i+1].isidentifier() or tokens[i+1].isnumeric()) :
-                    print("Error : Invalid expression syntax - missing operator between identifiers or numbers, at line " + str(line_number))
-                    sys.exit(1)
-
-    #Precedence dictionary
+     
+    # Precedence dictionary
     precedence = {
-    '!': 4 , 'defined': 4 , '!defined': 4, '*': 3, '/': 3, '%': 3, '+': 2, '-': 2, '<<': 1, '>>': 1,
-    '<': 0, '>': 0, '<=': 0, '>=': 0, '==': -1, '!=': -1, '&&': -2, '||': -3 , '&': -4 , '|': -5,
-    '(': -6 , ')': -6
-
+        'defined': 14, 
+        '~': 14,
+        '-u': 14,  
+        '+u': 14,  
+        
+        '!': 14,
+        '*': 13,
+        '/': 13,
+        '%': 13,
+    
+        
+        '+': 12,
+        '-': 12,
+    
+        
+        '<<': 11,
+        '>>': 11,
+    
+        
+        '<': 10,
+        '>': 10,
+        '<=': 10,
+        '>=': 10,
+    
+        
+        '==': 9,
+        '!=': 9,
+    
+        
+        '&': 8,
+        '^': 7,
+        '|': 6,
+    
+        
+        '&&': 5,
+        '||': 4
     }
 
     output = []
     stack = []
 
-    #build postfix expression
+    # build postfix expression AST
     for token in tokens:
-        if (token.isidentifier() or token.isnumeric()) and token not in ('defined','!defined'):
+        if (token.isidentifier() or token.isnumeric()) and token != 'defined':
             output.append(token)
         elif token == '(':
             stack.append(token)
@@ -90,178 +169,113 @@ def c_eval(expression,line_number):
             stack.pop()  # Pop the '(' from the stack
         elif token in precedence:
             while (stack and stack[-1] != '(' and
-                   precedence[stack[-1]] >= precedence[token]):
+                   precedence[stack[-1]] > precedence[token]):
                 output.append(stack.pop())
             stack.append(token)
     while stack:
         output.append(stack.pop())
-
+    
     # evaluate postfix expression
     eval_stack = []
     for token in output:
-        #Add guard for defined identifiers as 0
         if token.isnumeric():
-            # handle zero
-            if token == '0':
-                eval_stack.append(None)
-            else:
-                eval_stack.append(int(token))
-        elif token.isidentifier() and token not in ('defined','!defined'):
+            eval_stack.append(int(token))
+        elif token.isidentifier() and token != 'defined':
             eval_stack.append(0)  # Undefined identifiers are treated as 0
         elif token == '+':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a + b)
         elif token == '-':
-            #push only negative of the number
+            b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            eval_stack.append(-a)
+            eval_stack.append(a - b)           
         elif token == '*':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a * b)
         elif token == '/':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a // b)
         elif token == '%':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a % b)
         elif token == '<<':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a << b)
         elif token == '>>':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a >> b)
         elif token == '<':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a < b))
-        elif  token == '==':
+        elif token == '==':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a == b))
         elif token == '!=':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a != b))
-        elif token == '!defined':
-            a = eval_stack.pop()
-            if a is None:
-                eval_stack.append(0)
-            else:
-                eval_stack.append(int(a == 0))
         elif token == 'defined':
-            a = eval_stack.pop()
-            if a is None:
-                eval_stack.append(1)
-            else:
-              eval_stack.append(int(a != 0))
+            a = eval_stack.pop()           
+            eval_stack.append(0)
         elif token == '>':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a > b))
         elif token == '<=':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a <= b))
         elif token == '>=':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a >= b))
         elif token == '&&':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a and b))
         elif token == '||':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(int(a or b))
         elif token == '&':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a & b)
         elif token == '|':
             b = eval_stack.pop()
             a = eval_stack.pop()
-            if a is None:
-                a = 0
-            if b is None:
-                b = 0
             eval_stack.append(a | b)
         elif token == '!':
             a = eval_stack.pop()
-            if a is None:
-                a = 1
             eval_stack.append(int(not a))
-
+        elif token == '+u':
+            a = eval_stack.pop()
+            eval_stack.append(+a)
+        elif token == '-u':
+            a = eval_stack.pop()
+            eval_stack.append(-a)
+        elif token == '~':
+            a = eval_stack.pop()
+            eval_stack.append(~a)
+        elif token == '^':
+            b = eval_stack.pop()
+            a = eval_stack.pop()
+            eval_stack.append(a ^ b)
+    
     result = eval_stack.pop()
     output = True if result is None else result
-
-    #handle 0 case, octal, binary, hexadecimal numbers
     return bool(output)
+
+
+#if
+expr = "10 - 10 + 2 - 2"
+
+print(c_eval(expr))
